cda5b4b4106e1ffb6b5cd4a21b656283ea8ae9ac
Wen-Ying Wu <wenying@login2.stampede2.tacc.utexas.edu>
2022-03-02 01:38:47 -0600

    add readme file

---
+++ scripts/T4_WBA/README.md
@@ -0,0 +1,27 @@
+# Task4: Predict Naturalized Flow
+
+## Proposed method:
+Level1: water balance approach
+Level2: Level1 + bias correction
+
+## Level1
+In a given gauge, the flows is caculated from the summation of the runoff in its contributed area.
+Q = sum(runoff *Area)
+
+Negative runoff is mannualy replace by zero firstly.
+
+Output: ./data/output/T4_NCP_Flows_L1_mon_ACCESS-CM2_historical.csv
+
+![plot](./fig/T4_L1_Compare_K1.pdf)
+![plot](./fig/T4_L1_NF_K1.pdf)
+
+## Level2
+Here I tried to applied a CDF matching. I borrowed the code from 
+https://soilwater.github.io/pynotes-agriscience/notebooks/cdf_matching.html
+
+But I found it is might not be used for streamflow.
+I still share the figure/output here, but not the code.
+
+Output: ./data/output/T4_NCP_Flow_L2_mon_ACCESS-CM2_historical.csv
+
+![plot](./fig/T4_L2_NF_K1_ACCESS-CM2.pdf)
--- scripts/T4_WBA/plot_T4_L1.py
+++ scripts/T4_WBA/plot_T4_L1.py
@@ -9,72 +9,94 @@ import matplotlib.gridspec as gridspec
 import pandas as pd
 import numpy  as np
 
-
-
-## read in Watersheds information
-df_nf = pd.read_csv('./data/input/Naturalized_Flows.csv')
-## read in hydrograph
-y = np.array(df_nf["D1"].str.replace(",","").astype(float))
-y_cycle = np.average(y.reshape(int(y.size/12),12),0)
-
+## define select years
+start_yr= 1950
+end_yr  = 2014
+time_ax = pd.date_range('1950-01','2015-01', freq='M')
 ##
-y_num_zero= len(np.where(y==0.0)[0])
+
+## read in Watersheds information (Name)
+df_ws = pd.read_csv('./data/input/WAM_control_point_name.csv')
+df_ws = df_ws.sort_values(by=["CP_ID"], ascending=True)
+df_ws = df_ws.reset_index(drop=True)
+CP_ID    = df_ws.CP_ID
+CP_name = df_ws.Description
+ 
+
+## read in 43 Flows timeseries
+df_nf = pd.read_csv('./data/input/Naturalized_Flows.csv')
+df_nf = df_nf.loc[(df_nf['Year'] >= start_yr) & (df_nf['Year']<= end_yr)]
 
 
-
-## proccess the hydrograph to histogram (PDF without fitting)
 # prepare the bins
 range_min = 1.0
-range_max = 100000.0
+range_max = 10000000.0
 #range_max = np.percentile(y, 95)
 #range_max = y.max()
 bins_list = np.logspace(np.log10(range_min),np.log10(range_max),num=30)
-print(bins_list)
-#the fist bins is [0,x), including 1 but excluding x; 
+#the fist bins is [0,x), including 1 but excluding x;
 
 
-# histogram pdf(density); bin_edges= bins_list
-pdf, bin_edges = np.histogram(y,bins=bins_list, density=True)
-print(pdf)
-## pdf to cdf
-cdf = np.cumsum(pdf)
+for i in range(CP_ID.size):
 
+  ID = CP_ID[i]
+  print(ID)
 
-## plot sections
-## create 2*2 subplots
-fig = plt.figure(figsize=(10,6))
-gs = gridspec.GridSpec(2,3, wspace=.5, hspace=.5)
+  ## read in hydr graph
+  y = np.array(df_nf[ID].str.replace(",","").astype(float))
+  y_cycle = np.average(y.reshape(int(y.size/12),12),0)
 
-# first plot: monthly time series
-ax1= plt.subplot(gs[0,0:2])
-bins_list[1] = 1
-bins_list[1] = 1
-ax1.plot(y)
-ax1.set_title("Timeseries")
-# second plot: annual cycle
-ax2= plt.subplot(gs[0,2])
-ax2.plot(y_cycle)
-ax2.set_title("Annual Cycle")
-## third plot: count of zero streamflow
-ax3= plt.subplot(gs[1,0])
-ax3.bar(0,y_num_zero)
-ax3.set_title("Count of zero flow")
-
-## 4th plot: log space PDF
-ax4= plt.subplot(gs[1,1])
-ax4.plot(bin_edges[1:],pdf)
-ax4.set_xscale('log')
-ax4.set_title("PDF")
-
-## 5th plot: log space CDF
-ax5 = plt.subplot(gs[1,2])
-ax5.plot(bin_edges[1:],cdf)
-ax5.set_xscale('log')
-ax5.set_title("CDF")
-
-fig.suptitle("D1 Colorado River at Winchell", fontweight ="bold")
-
-plt.savefig('test.pdf')
+  ## count zero flow
+  y_num_zero= len(np.where(y==0.0)[0])
 
 
 
+  ## proccess the hydrograph to histogram (PDF without fitting)
+  bins_list = np.logspace(np.log10(range_min),np.log10(range_max),num=30)
+  #the fist bins is [0,x), including 1 but excluding x; 
+
+  # histogram pdf(density); bin_edges= bins_list
+  hist, bin_edges = np.histogram(y,bins=bins_list)
+  # count to probability
+  pdf = hist/y.size
+  ## pdf to cdf
+  cdf = np.cumsum(pdf)
+
+
+
+  ## plot sections
+  ## create 2*2 subplots
+  fig = plt.figure(figsize=(10,6))
+  gs = gridspec.GridSpec(2,3, wspace=.5, hspace=.5)
+
+  # first plot: monthly time series
+  ax1= plt.subplot(gs[0,0:2])
+  bins_list[1] = 1
+  bins_list[1] = 1
+  ax1.plot(time_ax,y)
+  ax1.set_title("Timeseries")
+  # second plot: annual cycle
+  ax2= plt.subplot(gs[0,2])
+  ax2.plot(y_cycle)
+  ax2.set_title("Annual Cycle")
+  ## third plot: count of zero streamflow
+  ax3= plt.subplot(gs[1,0])
+  ax3.bar(0,y_num_zero)
+  ax3.set_title("Count of zero flow")
+
+  ## 4th plot: log space PDF
+  ax4= plt.subplot(gs[1,1])
+  ax4.plot(bin_edges[1:],pdf)
+  ax4.set_xscale('log')
+  ax4.set_title("PDF")
+
+  ## 5th plot: log space CDF
+  ax5 = plt.subplot(gs[1,2])
+  ax5.plot(bin_edges[1:],cdf)
+  ax5.set_xscale('log')
+  ax5.set_title("CDF")
+
+  fig.suptitle(ID +": "+ CP_name[i], fontweight ="bold")
+
+  plt.savefig("./fig/T4_L1_NF_"+ID+'.pdf')
+  plt.close('all')
